b4a55f236a001f369efbfa97a4bf8c98597792b1
pengu <sellamB66@gmail.com>
2022-12-06 10:32:03 +0000

    testing wordpress script

---
+++ src/images/wordpress/conf/entry_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+chown -R www-data:www-data /var/www/* 
+chown -R 755 /var/www/* 
+echo "im here"
+wp core config --path=/var/www/html --dbname=db1 --dbuser=ybensell --dbpass=123456 --dbhost=maria --allow-root --skip-check 
+echo "conf success"
+wp core install --path=/var/www/html --url=localhost --title="INCEPTION" --admin_user=ybensell --admin_password=123456 --admin_email=hh@hotmail.com --allow-root 
+echo "install success"
+echo "hh"
