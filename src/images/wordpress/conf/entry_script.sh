820afe908f3e3d162a93c6ad445eb137cbebda25
pengu <sellamB66@gmail.com>
2022-12-06 12:11:27 +0000

    Configuring wordpress script

--- src/images/wordpress/conf/entry_script.sh
+++ src/images/wordpress/conf/entry_script.sh
@@ -4,6 +4,6 @@ chown -R 755 /var/www/*
 echo "im here"
 wp core config --path=/var/www/html --dbname=db1 --dbuser=ybensell --dbpass=123456 --dbhost=maria --allow-root --skip-check 
 echo "conf success"
-wp core install --path=/var/www/html --url=localhost --title="INCEPTION" --admin_user=ybensell --admin_password=123456 --admin_email=hh@hotmail.com --allow-root 
+wp core install --path=/var/www/html --url=localhost --title="INCEPTION" --admin_user=ybensell --admin_password=123456 --admin_email=hh@hotmail.com --allow-root
+wp user create wpp  wpp@gmail.com --user_pass=123456 --path=/var/www/html --allow-root
 echo "install success"
-echo "hh"
